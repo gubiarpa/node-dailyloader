ab0093b2c2aa076f69449f2d8b3ebc6947179bc0
Billy Arredondo <billy.arredondo@pucp.pe>
2020-03-27 02:53:43 -0500

    Fields Management - Parte I
    Field recognition and separation of success and failure in matches

--- src/utils/fileManager.js
+++ src/utils/fileManager.js
@@ -9,15 +9,17 @@ const processFile = (config, content) => {
 
     /* III. Valid header (ej. 'Province/State,Country/Region,Last Update,Confirmed' */
     let header = lines[0].trim();
-    let headerValidation = ValidateHeader(config.fields, header);
-    if (!headerValidation.isValid){
-        console.log({
-            result: 'The fields in the upload file do not match the required fields.',
-            detail: headerValidation.errorArray
-        });
-        return;
-    }
+    let headerValidation = ValidateHeader(config.definedFields, header);
 
+    console.log('•', headerValidation);
+    if (!headerValidation.isValid){
+        return {
+            result: 'The fields in the upload file do not match the required fields.',
+            matchDetail: headerValidation.matchArray,
+            errorDetail: headerValidation.errorArray
+        };
+    }
+    return;
     /* IV. Read body */
     for (const i in lines) {
         if (i > 0) {
@@ -26,33 +28,50 @@ const processFile = (config, content) => {
     }
 }
 
-const ValidateHeader = (requiredFields, header) => {
+const ValidateHeader = (definedFields, header) => {
+    // (array, string) => {...}
     
-    let isValid;
-    let errorArray = [];
+    let isValid = false; // ¿Cabecera válida?
+    let matchArray = []; // Lista campos con posiciones en CSV
+    let errorArray = []; // Lista campos no encontrados
 
     try {
 
-        // Split by comma (ej. ['Country', 'Province'])
-        let fields = header.split(',');
-        
-        for (const requiredfield of requiredFields) {
-            // Does header line have the requiredField?
-            if (
-                (requiredfield.required) &&
-                (fields.find((e) => e == requiredfield.name) == null)
-                ) errorArray.push(requiredfield.name);
+        let fields = header.split(','); // fields from file
+
+        for (const definedfield of definedFields) {
+            // Ej. definedField.csvMatchName = ['Province/State', 'Province_State', 'Prov_Stat']
+            
+            if (definedfield.required == true) {
+
+                let founded = false, matchPosition = -1, i = 0;
+
+                while ((!founded) && (i < definedfield.csvMatchName.length)) {
+                    let matchItem = definedfield.csvMatchName[i++];
+                        // ej. matchItem = 'Province_State'
+                    matchPosition = fields.findIndex((e) => e == matchItem);
+                    founded = (matchPosition > -1);
+                }
+
+                if (founded) {
+                    matchArray.push({ name: definedfield.name, index: matchPosition});
+                } else {
+                    errorArray.push(definedfield.name);
+                }
+            }
         }
-    
+
         isValid = (errorArray.length == 0);
 
     } catch (error) {
         isValid = false;
+        matchArray = [];
         errorArray = [error];
     }
 
     return {
         isValid,
+        matchArray,
         errorArray
     }
 }
